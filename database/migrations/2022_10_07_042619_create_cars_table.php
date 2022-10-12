21dc67f75710af3811f70fb8346f6730321d634d
黃立旻 <kevin200129@gmail.com>
2022-10-12 17:40:07 +0800

    這是我的第一個版本控制

---
+++ database/migrations/2022_10_07_042619_create_cars_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->integer('id')->unsigned()->comment('編號');
+            $table->string('type',191)->nullable(false)->comment('車型');
+            $table->tinyInteger('bid')->unsigned()->nullable(false)->comment('品牌');
+            $table->integer('horsepower')->nullable(true)->comment('馬力');
+            $table->integer('torque')->nullable(true)->comment('扭力');
+            $table->integer('cc')->nullable(false)->comment('CC數');
+            $table->double('money')->unsigned()->nullable(false)->comment('價錢');
+            $table->double('variable_s')->unsigned()->nullable(false)->comment('變速系統');
+            $table->tinyInteger('seats')->unsigned()->nullable(true)->comment('座位');
+            $table->string('nationality',191)->nullable(false)->comment('國家');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
